3ea7815b151afdd7b38fa71c4f853c62a9a35719
GurjotSingh84 <gurjot.singh8229@nctorontostudents.ca>
2025-03-19 22:51:32 -0400

    Add files via upload

---
+++ TalentLink_app/Models/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentLink_app.Models
+{
+    public class Notification
+    {
+        public string CandidateId { get; set; }
+        public string RecruiterId { get; set; }
+        public string JobId { get; set; }
+        public string Status { get; set; } // Approved / Rejected
+        public string Message { get; set; }
+        public string Timestamp { get; set; }
+    }
+}
