9bf2011a4ac58aef64a8e41794595b8d86eb34bf
GurjotSingh84 <gurjot.singh8229@nctorontostudents.ca>
2025-03-19 22:12:49 -0400

    Add files via upload

---
+++ TalentLink_app/Models/CandidateProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentLink_app.Models
+{
+    public class CandidateProfile
+    {
+        public string CandidateId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Skills { get; set; }
+        public int Experience { get; set; }
+        public string Location { get; set; }
+    }
+
+}
